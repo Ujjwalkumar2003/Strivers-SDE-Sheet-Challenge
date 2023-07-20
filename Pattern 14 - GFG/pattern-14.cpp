9a6b862c842f1783b21bd889833b5feb339cfe0b
Ujjwa Kumar <76715464+Ujjwalkumar2003@users.noreply.github.com>
2023-07-20 14:23:16 +0530

    Added solution - LeetHub

--- Pattern 14 - GFG/pattern-14.cpp
+++ Pattern 14 - GFG/pattern-14.cpp
@@ -11,9 +11,11 @@ class Solution {
          
         for(int i=0;i<n;i++)
         {
-            for(char j='A';j<='A'+i;j++)
+            char ch='A';
+            for(int j=0;j<=i;j++)
             {
-                cout<<j;
+                cout<<ch;
+                ch++;
             }
             cout<<endl;
         }
