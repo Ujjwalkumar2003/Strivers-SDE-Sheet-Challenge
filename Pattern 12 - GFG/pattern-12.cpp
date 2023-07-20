c34739894da641537b598de18a14af4207e6392f
Ujjwa Kumar <76715464+Ujjwalkumar2003@users.noreply.github.com>
2023-07-20 13:55:23 +0530

    Added solution - LeetHub

---
+++ Pattern 12 - GFG/pattern-12.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        int N=n;
+  int spaces = 4*(N-1);
+      
+      // Outer loop for the number of rows.
+      for(int i=1;i<=N;i++){
+          
+          // for printing numbers in each row
+          for(int j=1;j<=i;j++){
+              cout<<j<<" ";
+          }
+          
+          // for printing spaces in each row
+          for(int j = 1;j<=spaces;j++){
+              cout<<" ";
+          }
+          
+          // for printing numbers in each row
+          for(int j=i;j>=1;j--){
+              cout<<j<<" ";
+          }
+          
+          // As soon as the numbers for each iteration are printed, we move to the
+          // next row and give a line break otherwise all numbers
+          // would get printed in 1 line.
+          cout<<endl;
+          
+          // After each iteration nos. increase by 2, thus
+          // spaces will decrement by 2.
+          spaces-=4;
+      }
+        
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
